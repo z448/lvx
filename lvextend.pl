efc79420c699cd57c220577e6d40b3228d9f6a78
Zdenek <zed448@icloud.com>
2017-01-26 02:55:47 -0800

    make new pv

--- lvextend.pl
+++ lvextend.pl
@@ -24,36 +24,51 @@ my $mount_point =  sub {
     $m{disk} = $m{pv}->[0]; $m{disk} =~ s/[0-9]+//g;
     return \%m;
 };
-#
-##my @opt = qw< p n p t 2 w 8e w >;
 
-sub extend {
-    my $m = $mount_point->(shift);
-    #print Dumper $m; #test
-    my $extend = <<"_448";
-        echo "n
-        n
-        1
+my $extend = sub {
+	my $path = shift;
+    	my $m = $mount_point->($path);
+    	print Dumper $m; #test
+	#my $p = qq{echo -e "n\n\n\n\nt\n8e\nw\n" |fdisk $m->{disk}};
+	#system("partprobe $m->{disk}");
+
+	my @pv = @{ $m->{pv} };
+	my $last_pv = $pv[$#pv]; 
+	my $new_pv = $last_pv;
+	$new_pv =~ /(.*?)([0-9]).*/;
+	my($pv, $nr) = ($1, $2); $nr++; $pv = $pv . $nr;
+	say $pv;
+	#system("pvcreate $pv");
+	#system("vgextend $m->{vg} $m->{$pv}");
+	#system("lvextend -l +100% $m->{}");
+	#system("resize2fs /dev/vg_repodata/lv_big");
+};
+
+my $e = $extend->("$ARGV[0]");
 
 
-        "|fdisk $m->{disk}
 
-_448
 
-    system("$extend");
-}
 
-extend("$ARGV[0]");
 __DATA__
 
 create partition sdb1 for full current space 5G
 initialize pv to use with lvm (pvcreate /dev/sdb1)
 create lv vg on sdb1 full extend to disk size(5G) (vgcreate vg_repodata /dev/sdb1; lvcreate -n lv_big -l 100%FREE vg_repodata)
 make filesystem type (mkfs.ext4 /dev/vg_repodata/lv_big)
+mount lv to mountpoint (mount /dev/vg_repodata/lv_big /big)
+
 ----
 extend disk in vm to 7G
+
+sdb                      8:16   0    7G  0 disk
+└─sdb1                   8:17   0    5G  0 part
+  └─vg_repodata-lv_big 252:0    0    5G  0 lvm
 ----
+
 create partition sdb2 for remainging space 2G
+initialize pv to use with lvm (pvcreate /dev/sdb2)
+force kernel to use new part table (partprobe /dev/sdb)
 Add this pv to vg_tecmint vg to extend the size of a volume group to get more space for expanding lv(vgextend vg_tecmint /dev/sda2)
 check available Physical Extends( available Physical Extend )
 expand lv(lvextend -l +4607 /dev/vg_repodata/lv_big)
